af0c91c26d7b529ae900f2b733b20fde87e4ff20
Oscar Espitia <odespitia@sena.edu.co>
2023-07-01 01:03:35 -0500

    actividad didactica

--- src/config/global.js
+++ src/config/global.js
@@ -85,12 +85,12 @@ export default {
         nombreRuta: 'sintesis',
         desarrolloContenidos: true,
       },
-      // {
-      //   nombreRuta: 'actividad',
-      //   icono: 'far fa-question-circle',
-      //   titulo: 'Actividad didáctica',
-      //   desarrolloContenidos: true,
-      // },
+      {
+        nombreRuta: 'actividad',
+        icono: 'far fa-question-circle',
+        titulo: 'Actividad didáctica',
+        desarrolloContenidos: true,
+      },
       {
         nombreRuta: 'glosario',
         icono: 'fas fa-sort-alpha-down',
